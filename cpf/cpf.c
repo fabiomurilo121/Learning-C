72af0f7363238cfc0f3c998c39b7576ebcd42835
fabio <fabio.cagni@gmail.com>
2020-08-24 20:49:35 -0300

    soma

--- cpf/cpf.c
+++ cpf/cpf.c
@@ -7,9 +7,10 @@
 #include <stdlib.h>
 
 #define TAM 14
+#define TAMINT 11
 
-char aux[1], cpf[TAM] = "101.171.639-96";
-int i, cpfInt[11];
+char aux[1], cpf[TAM] = "123.456.789-12";
+int cpfInt[TAMINT],soma[9], j, k, i;
 
 void removeEspacos(char* s) {
     const char* d = s;
@@ -21,6 +22,7 @@ void removeEspacos(char* s) {
 }
 
 void removeDivicao(char entrada[]){
+    int i;
     for(i=0; i< strlen(entrada); ++i){
         if(entrada[i]=='.' || entrada[i]=='-'){
             entrada[i] = ' ';
@@ -31,18 +33,21 @@ void removeDivicao(char entrada[]){
 int main(int argc, char**argv){
 
     printf("CPF de entrada foi: %s\n",cpf);
-
     removeDivicao(cpf);
     removeEspacos(cpf);
 
-
-    for (int j = 0; j < strlen(cpf); ++j) {
+    for (j = 0; j < strlen(cpf); ++j) {
         aux[0] = cpf[j];
         cpfInt[j] = (int) atoi(aux);
     }
 
-    for (int j = 0; j < strlen(cpf); ++j) {
-        printf("%d",cpfInt[j]);
+    for (k = 10, i = 0; k >= 2, i <= 9; --k, ++i) {
+
+        soma[i] = (cpfInt[i] * k);
+    }
+
+    for (int l = 0; l < 9; ++l) {
+        printf("%d\n",soma[l]);
     }
 
     return 0;
